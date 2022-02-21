4d8db0ba1b50147f8f83db658bbffa506ac9e256
yeonil <ahnyi527@gmail.com>
2022-02-21 15:39:37 +0900

    Initial commit

---
+++ servlcet/src/main/java/hello/servlcet/ServlcetApplication.java
@@ -0,0 +1,13 @@
+package hello.servlcet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServlcetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServlcetApplication.class, args);
+	}
+
+}
---
+++ servlcet/src/test/java/hello/servlcet/ServlcetApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.servlcet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServlcetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
