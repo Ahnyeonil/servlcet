f8c9d066a56d518657c7ab5f35f0ddfa17fd74ac
yeonil <ahnyi527@gmail.com>
2022-02-22 15:51:35 +0900

    FrontController_v1

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
@@ -1,19 +1,18 @@
-package hello.servlet.web.servletmvcc;
+package hello.servlet.web.frontcontroller.v1.controller;
+
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "mvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
-public class MvcMemberFormServlet extends HttpServlet {
+public class MemberFormControllerV1 implements ControllerV1 {
+
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // WEB-INF -> 외부에서 호출해도 호출되지 않는다.
         String viewPath = "/WEB-INF/views/new-form.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
@@ -22,4 +21,3 @@ public class MvcMemberFormServlet extends HttpServlet {
         dispatcher.forward(request, response);
     }
 }
-
